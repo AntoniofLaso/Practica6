44892ce0d4a9acd72a7367ecc54ba74761881f8e
AntoniofLaso <antoniofdzlaso@hotmail.com>
2025-12-16 17:58:09 +0100

    Add files via upload

---
+++ LungScanAI/src/main/java/ZA/LungScanAI/LungScanAiApplication.java
@@ -0,0 +1,13 @@
+package ZA.LungScanAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LungScanAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LungScanAiApplication.class, args);
+	}
+
+}
