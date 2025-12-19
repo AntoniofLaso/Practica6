73f889c06ce50fbbfdda5134dde33464594782b6
AntoniofLaso <antoniofdzlaso@hotmail.com>
2025-12-19 15:13:01 +0100

    Delete LungScanAI directory

--- LungScanAI/src/main/java/ZA/LungScanAI/DiagnosisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.LungScanAI.repository;
-
-import com.proyecto.LungScanAI.model.DiagnosisReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiagnosisRepository extends JpaRepository<DiagnosisReport, Long> {
-}
